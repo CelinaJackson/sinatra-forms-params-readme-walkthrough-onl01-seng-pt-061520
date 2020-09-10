f532b90105f121ae3c76eb74902cb274ddd21dc9
Celina Jackson <celinanjackson@gmail.com>
2020-09-10 19:19:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,10 +5,6 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
-  
-   params = {
-        :name => "Sam",
-        :favorite_food => "Green Eggs and Ham"}
         
   post '/food' do 
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
