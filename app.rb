1fd6de0dc9f5714fc6904c8ef9993fbbb87a0510
Celina Jackson <celinanjackson@gmail.com>
2020-09-10 19:23:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,7 @@ class App < Sinatra::Base
   end
         
   post '/food' do 
-    name = params[:name] 
-    favorite_food = params[:favorite_food]
+    
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
 
